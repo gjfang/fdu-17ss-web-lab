5d0530daecd2dc11950e8fc666b2c9afc18e0c26
gjfang <37617449+gjfang@users.noreply.github.com>
2018-05-27 16:51:30 +0800

    Add files via upload

---
+++ Lab09.php
@@ -0,0 +1,106 @@
+<?php
+include 'travel-data.inc.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Chapter 12</title>
+
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    
+    
+    <link rel="stylesheet" href="css/bootstrap-theme.css" />
+    <link rel="stylesheet" href="css/captions.css" />
+
+</head>
+
+<body>
+    <?php include 'header.inc.php'; ?>
+    
+
+
+    <!-- Page Content -->
+    <main class="container">
+     
+        
+        <div class="btn-group countryButtons" role="group" aria-label="...">
+              <a role="button" class="btn btn-default" href="list.php">All</a>
+           
+              <?php   
+              /* you will need to fill this place with appropriate PHP */
+              /* hint: use array and loop */
+              //the next line is an example
+              //<a href="list.php?country=Canada" role="button" class="btn btn-default"> Canada</a>
+              $M=${"countries"};
+              reset($M);
+              while(list($short,$name) = each($M)){
+                  echo "<a href='list.php?country=".$name."' role=\"button\" class=\"btn btn-default\">".$name."<a/>";
+              }
+              ?>
+                     
+        </div>               
+           
+        
+
+		<ul class="caption-style-2">
+         
+          <?php   
+          /* you will need to fill this place with appropriate PHP */ 
+          /* hint: use array and loop */
+          //the next lines are one of the elements
+          //you need to fill the elements with data provided
+//
+//           $N=${"images"};
+//           reset($N);
+//           while(list($key,$id,$title,$description,$country,$city,$user,$path,$taken,$tags)=each($N)){
+//               $div0="";
+//               $div0.="<li><a href='detail.php?id=".$id."' class=\"img-responsive\">";
+//              $div0.="<img src='images/square/".$path."' alt='".$title."'>";
+//              $div0.="<div class=\"caption\"><div class=\"blur\"></div><div class=\"caption-text\"><h1>".$title."</h1></div></div></a>";
+//              $div0.="</li>";
+//               echo $div0;
+//           }
+          $div0="";
+          $my=array("22","54","7","19","46","6","60","58","75","77","27","2","24","13","101","102");
+          for ($i=0;$i<count($my);$i++){
+
+              $div0.="<li><a href='detail.php?id=".${"images"}[$my[$i]]["id"]."' class=\"img-responsive\">";
+              $div0.="<img src='images/square/".${"images"}[$my[$i]]["path"]."' alt='".${"images"}[$my[$i]]["title"]."'>";
+              $div0.="<div class=\"caption\"><div class=\"blur\"></div><div class=\"caption-text\"><h1>".${"images"}[$my[$i]]["title"]."</h1></div></div></a>";
+              $div0.="</li>";
+
+
+          }
+
+          echo $div0;
+          ?>
+
+       </ul>       
+
+      
+    </main>
+    
+    <footer>
+        <div class="container-fluid">
+                    <div class="row final">
+                <p>Copyright &copy; 2017 Creative Commons ShareAlike</p>
+                <p><a href="#">Home</a> / <a href="#">About</a> / <a href="#">Contact</a> / <a href="#">Browse</a></p>
+            </div>            
+        </div>
+        
+
+    </footer>
+
+
+        <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+</body>
+
+</html>
